a4d1b5e8c5db626c0980f661997c3f86f6864bcc
Morten Hersson <mhersson@gmail.com>
2023-02-23 15:03:57 +0000

    Commit from devspaces

--- main.go
+++ main.go
@@ -2,39 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
-	"time"
 )
 
 func checkError(w *http.ResponseWriter, msg string, err error) {
 	if err != nil {
 		fmt.Fprintf(*w, "%s: %s\n", msg, err)
 	}
-}
-
-func getQuote() (string, error) {
-	client := http.Client{
-		Timeout: 250 * time.Millisecond, //nolint: go-lint
-	}
-
-	req, _ := http.NewRequest("GET", "http://hello-quotes", nil)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
 }
 
 func simpleHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,7 +26,8 @@ func simpleHandler(w http.ResponseWriter, r *http.Request) {
 	interfaces, err := net.Interfaces()
 	checkError(&w, "Failed to get interfaces", err)
 
-	fmt.Fprintf(w, "Hello!\nMy name is %s!\n", hostname)
+	fmt.Fprintln(w, "Hello from devspaces")
+	fmt.Fprintf(w, "My workspace is %s!\n", hostname)
 	fmt.Fprintln(w, "You can find me here:")
 
 	for _, iface := range interfaces {
@@ -58,11 +35,6 @@ func simpleHandler(w http.ResponseWriter, r *http.Request) {
 		if len(addr) > 0 && iface.Name != "lo" {
 			fmt.Fprintf(w, "%s - %s\n", iface.Name, addr[0])
 		}
-	}
-
-	quote, err := getQuote()
-	if err == nil {
-		fmt.Fprint(w, quote)
 	}
 }
 
